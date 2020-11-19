9c5fdf33d8ad54f83d3a6f3641aa5f3ddc7a56c0
Geralt555 <longer2@yandex.ru>
2020-11-19 09:22:13 +0300

    Задачи на js

---
+++ 1.js
@@ -0,0 +1,4 @@
+var str = 'abcde';
+alert(str[0]); //выведем букву 'a'
+alert(str[1]); //выведем букву 'b'
+alert(str[4]); //выведем букву 'e'
---
+++ 2.js
@@ -0,0 +1,8 @@
+var num = 1;
+num = num + 12;
+num = num - 14;
+num = num * 5;
+num = num / 7;
+num = num + 1;
+num = num - 1;
+alert(num);
